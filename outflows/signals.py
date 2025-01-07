75c1d79c309eaa8117db188d15a4e4309c44a887
guikastner <guilherme@kastner.com.br>
2025-01-07 10:18:37 -0300

    ajuste do signals

--- outflows/signals.py
+++ outflows/signals.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Outflow
@@ -16,8 +17,13 @@ def update_product_quantity(sender, instance, created, **kwargs):
 def send_outflow_event(sender, instance, **kwargs):
     notify = Notify()
     data={
-        'product': str(instance.product),
-        'quantity': instance.quantity
+        'event_type': 'outflow',
+        'timestamp': datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
+        'product': instance.product.title,
+        'product_selling_price': float(instance.product.selling_price), # Converte para float para evitar problemas com o jsoninstance.product.selling_price,
+        'product_cost_price': float(instance.product.cost_price), # Converte para float para evitar problemas com o jsoninstance.product.cost_price,
+        'quantity': instance.quantity,
+        'description': instance.description
     }
     notify.send_event(data)
     print("Enviou dados para o webhook")
